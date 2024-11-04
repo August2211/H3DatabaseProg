e13484f83c501b5d5727a1c655d6a37d53ff5579
August2211 <53059031+August2211@users.noreply.github.com>
2024-11-04 10:29:13 +0100

    2.1 glemte at SeedTasks skulle være en statisk metode

--- H3DatabaseProg/Program.cs
+++ H3DatabaseProg/Program.cs
@@ -2,39 +2,44 @@
 using System;
 using System.Linq;
 
-using var db = new BloggingContext();
+static void SeedTasks()
+{
+    using var db = new BloggingContext();
 
-// Note: This sample requires the database to be created before running.
-Console.WriteLine($"Database path: {db.DbPath}.");
+    // Note: This sample requires the database to be created before running.
+    Console.WriteLine($"Database path: {db.DbPath}.");
 
-Task ProduceSoftwareTask = new();
-ProduceSoftwareTask.Name = "Produce Software";
-ProduceSoftwareTask.Todos = new()
+    Task ProduceSoftwareTask = new();
+    ProduceSoftwareTask.Name = "Produce Software";
+    ProduceSoftwareTask.Todos = new()
     {
         new Todo { Name = "Write code", IsComplete = true },
         new Todo { Name = "Compile source", IsComplete = true },
         new Todo { Name = "Test program", IsComplete = false },
     };
 
-Task BrewCoffeeTask = new();
-BrewCoffeeTask.Name = "Brew Coffee";
-BrewCoffeeTask.Todos = new()
+    Task BrewCoffeeTask = new();
+    BrewCoffeeTask.Name = "Brew Coffee";
+    BrewCoffeeTask.Todos = new()
     {
         new Todo { Name = "Pour water", IsComplete = true },
         new Todo { Name = "Pour coffee", IsComplete = true },
         new Todo { Name = "Turn on", IsComplete = true },
     };
 
-db.Tasks.Add(ProduceSoftwareTask);
-db.Tasks.Add(BrewCoffeeTask);
-db.SaveChanges();
+    db.Tasks.Add(ProduceSoftwareTask);
+    db.Tasks.Add(BrewCoffeeTask);
+    db.SaveChanges();
 
-var tasks = db.Tasks.Include(task => task.Todos);
-foreach (var task in tasks)
-{
-    Console.WriteLine($"Task: {task.Name}");
-    foreach (var todo in task.Todos)
+    var tasks = db.Tasks.Include(task => task.Todos);
+    foreach (var task in tasks)
     {
-        Console.WriteLine($"- {todo.Name} is {(todo.IsComplete ? "complete" : "incomplete")}");
+        Console.WriteLine($"Task: {task.Name}");
+        foreach (var todo in task.Todos)
+        {
+            Console.WriteLine($"- {todo.Name} is {(todo.IsComplete ? "complete" : "incomplete")}");
+        }
     }
 }
+
+SeedTasks();
